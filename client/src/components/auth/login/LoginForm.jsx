c4aef51a5657fe03231b9e054d4f18a0a4d7a508
Pramod_Acharya <pramodacharya808@gmail.com>
2024-08-24 02:09:12 +0530

    updated - 1

--- client/src/components/auth/login/LoginForm.jsx
+++ client/src/components/auth/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import "./LoginForm.css";
+// import "./LoginForm.css";
 import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
