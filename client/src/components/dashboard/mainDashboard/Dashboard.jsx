41c45b4ffa1cc0b1899d89da6f421c470b118c73
Pramod_Acharya <pramodacharya808@gmail.com>
2024-08-24 14:06:22 +0530

    added quiz metrix in dashboard

--- client/src/components/dashboard/mainDashboard/Dashboard.jsx
+++ client/src/components/dashboard/mainDashboard/Dashboard.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Dashboard.css";
 import { useAuth } from "../../../context/AuthContext";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link, Outlet } from "react-router-dom";
-import Analytics from "../analytics/allAnalytics/Analytics";
+import axios from "axios";
 
 const Dashboard = () => {
   const { logout } = useAuth();
+
+  const [totalQuiz, setTotalquiz] = useState();
+  const [totalQuestions, setTotalquestions] = useState(0);
+  const [totalImpressions, setTotalimpressions] = useState(0);
+
+  function formatNumber(num) {
+    if (num >= 1000000) {
+      return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (num >= 1000) {
+      return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    }
+    return num.toString();
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const totalquiz = await axios.get("/api/analytics/totalQuiz");
+      const totalquestions = await axios.get("/api/analytics/totalquestions");
+      const totalimpressions = await axios.get(
+        "/api/analytics/totalimpressions"
+      );
+
+      const quiz = formatNumber(totalquiz.data.data);
+      const questions = formatNumber(totalquestions.data.data);
+      const impressions = formatNumber(totalimpressions.data.data);
+      setTotalquiz(quiz);
+      setTotalquestions(questions);
+      setTotalimpressions(impressions);
+    }
+    getData();
+  }, []);
 
   return (
     <div className="container">
@@ -38,6 +70,38 @@ const Dashboard = () => {
       </div>
       <div className="right-section bg-primary">
         <Outlet />
+        {/* below is my main dashboard code */}
+        <div className="right-section-container">
+          <div className="top-section">
+            <div className="card-1">
+              <div className="top-card">
+                <h1 className="count">{totalQuiz}</h1>
+                <p className="count-label">Quiz</p>
+              </div>
+              <div className="bottom-card">
+                <p className="card-info">Created</p>
+              </div>
+            </div>
+            <div className="card-2">
+              <div className="top-card">
+                <h1 className="count">{totalQuestions}</h1>
+                <p className="count-label">Questions</p>
+              </div>
+              <div className="bottom-card">
+                <p className="card-info">Created</p>
+              </div>
+            </div>
+            <div className="card-3">
+              <div className="top-card">
+                <h1 className="count">{totalImpressions}</h1>
+                <p className="count-label">Total</p>
+              </div>
+              <div className="bottom-card">
+                <p className="card-info">Impressions</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
