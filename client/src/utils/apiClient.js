1613eb516705d3b1d158cad708fc145bdb6a98cf
Pramod_Acharya <pramodacharya808@gmail.com>
2024-10-05 01:42:55 +0530

    changed backend api url

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -3,7 +3,8 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "http://localhost:8000/api/v1", // Set your base API URL here
-  baseURL: "https://quizzie-cuvette-backend.onrender.com/api/v1", // Set your base API URL here
+  baseURL: "https://quizzie-cuvette-project-backend.onrender.com/api/v1", // Set your base API URL here
+  // baseURL: "https://quizzie-cuvette-backend.onrender.com/api/v1", // Set your base API URL here
 });
 
 apiClient.interceptors.request.use(
